d3b16874fd7bbf1fadbc98a2355bc5f93663a2a7
hcharp <ln_de_troie@hotmail.fr>
2021-07-14 12:16:13 +0200

    evol e2

--- charpentier_exo2.c
+++ charpentier_exo2.c
@@ -4,23 +4,43 @@
 pthread_t num_thread1;
 pthread_t num_thread2;
 
+// classe struc avec deux attributs parce que sinon j'étais obligée de faire deux fonctions et c'était moche
+struct MyThreadsStruct {
+    char* msg;
+    int zero_ou_un;
+};
 
-void my_message1() {
 
-    printf("Un message du thread numéro %d\n", num_thread1);
-}
+void my_message(struct MyThreadsStruct * une_structure) {
 
-void my_message2() {
-
-    printf("Un message du thread numéro %d\n", num_thread2);
+    if ((*une_structure).zero_ou_un == 0) { // on peut aussi faire avec le raccourci ->zero_ou_un
+        fprintf(stdout, "Message : %s\n", une_structure->msg);// c'est plus lisible c'est bo
+    } else {
+        fprintf(stderr, "Message : %s\n", une_structure->msg);
+        }
 }
 
 
 int main() {
 
+    char message1[] = "je suis a et j'en ai marre";
+    char message2[] = "zut";
 
+    struct MyThreadsStruct struct1 = {
+        message1,
+        0
+    };
+
+    struct MyThreadsStruct struct2 = {
+        message2,
+        1
+    };
+    
     // Le thread principal lance 2 threads auxiliaires
     // chacun un message
-    pthread_create(&num_thread1, NULL, (void *(*)())my_message1, NULL);
-    pthread_create(&num_thread2, NULL, (void *(*)())my_message2, NULL);
+    pthread_create(&num_thread1, NULL, (void *(*)())my_message, &struct1);
+    pthread_create(&num_thread2, NULL, (void *(*)())my_message, &struct2);
+
+    pthread_join(num_thread1, NULL);
+    pthread_join(num_thread2, NULL);
 }
