bc609383f3f7e7ca71ce05e617bab6f0a31359c9
hcharp <ln_de_troie@hotmail.fr>
2021-07-14 11:22:33 +0200

    blocked on e2

--- charpentier_exo2.c
+++ charpentier_exo2.c
@@ -9,12 +9,11 @@ void my_message(int num) {
 
 
 int main() {
-    int num_thread1;
-    int num_thread2;
-
+    pthread_t num_thread1;
+    pthread_t num_thread2;
 
     // Le thread principal lance 2 threads auxiliaires
     // avec un message chacun
-    pthread_create(&num_thread1, NULL, void*(*)())my_message, &num_thread1);
-    pthread_create(&num_thread2, NULL, void*(*)())my_message, &num_thread2);
+    int pthread_create(&num_thread1, NULL, void*(*my_message), &num_thread1);
+    int pthread_create(&num_thread2, NULL, void*(*my_message), &num_thread2);
 }
